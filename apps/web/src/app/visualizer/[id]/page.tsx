e12af579aa1ef79d1c52944c89fa4a6c583730fe
DOMinator.jsx <parthbrajput30@gmail.com>
2026-02-16 00:31:48 +0530

    HOTFIX : Fixed the dynamic routing for now.

--- apps/web/src/app/visualizer/[id]/page.tsx
+++ apps/web/src/app/visualizer/[id]/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const { id } = await params;
-  return <div>This is visualizer page. and this is the id: {id}</div>;
+const page = async () => {
+  // const { id } = await params;
+  return <div>This is visualizer page. and this is the id</div>;
 };
 
 export default page;
